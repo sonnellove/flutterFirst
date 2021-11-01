03b5876716096d0f1e0f332bedab0326741da993
unknown <heylovenelson@gmail.com>
2021-11-01 18:28:21 +0800

    Working_on_the_Products_Grid_Item_Widgets

---
+++ android/app/src/main/kotlin/com/example/fourth_appshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fourth_appshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  // const ProductItem({Key? key}) : super(key: key);
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: const Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
