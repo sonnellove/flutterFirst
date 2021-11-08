cff5fa43034309957fa9f24a564321f7e9f4b7b5
unknown <heylovenelson@gmail.com>
2021-11-08 21:15:28 +0800

    Listening_in_Different_Places_and_Ways

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // const ProductDetailScreen({Key? key}) : super(key: key);
+  // final String title;
+  // final double price;
+  // ProductDetailScreen(
+  //   this.title,
+  //   this.price,
+  // );
+
+  static const routeName = '/product-detail';
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title),
+      ),
+    );
+  }
+}
